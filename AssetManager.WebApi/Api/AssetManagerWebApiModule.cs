7715eef22afdbd66f6ed7c29aa46c56b5c9191d8
Jim Christian <jamesrchristian@hotmail.com>
2016-04-11 15:32:37 -0700

    Added Swagger support per http://www.aspnetboilerplate.com/Pages/Documents/Swagger-UI-Integration

--- AssetManager.WebApi/Api/AssetManagerWebApiModule.cs
+++ AssetManager.WebApi/Api/AssetManagerWebApiModule.cs
@@ -5,6 +5,8 @@ using Abp.Configuration.Startup;
 using Abp.Modules;
 using Abp.WebApi;
 using Abp.WebApi.Controllers.Dynamic.Builders;
+using Swashbuckle.Application;
+using System.Linq;
 
 namespace AssetManager.Api
 {
@@ -20,6 +22,20 @@ namespace AssetManager.Api
                 .Build();
 
             Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+
+            ConfigureSwaggerUi();
+        }
+
+        // Goes along with the Swashbuckle.Core nuget package. See http://www.aspnetboilerplate.com/Pages/Documents/Swagger-UI-Integration.
+        private void ConfigureSwaggerUi()
+        {
+            Configuration.Modules.AbpWebApi().HttpConfiguration
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "SwaggerIntegrationDemo.WebApi");
+                    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                })
+                .EnableSwaggerUi();
         }
     }
 }
