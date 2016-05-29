61d2bc5edc322d5ef97d0af79028c563c096cb8c
Jim Christian <jamesrchristian@hotmail.com>
2016-05-28 19:29:43 -0700

    Working modal dialog in the new "assetvariablelist" page. Modal dialog works; doesn't do anything useful (yet).

--- AssetManager.Web/App/Main/views/assethealth/assetvariablelist.js
+++ AssetManager.Web/App/Main/views/assethealth/assetvariablelist.js
@@ -2,11 +2,13 @@
     var app = angular.module('app');
 
     var controllerId = 'app.views.assethealth.assetvariablelist';
+    var modalControllerId = 'app.views.assethealth.assetvariablelist.modal';
     app.controller(controllerId, [
-        '$scope', '$location', 'abp.services.app.asset', 'abp.services.app.assetHealth',
-        function ($scope, $location, assetService, assetHealthService) {
+        '$scope', '$location', '$uibModal', 'abp.services.app.asset', 'abp.services.app.iowVariable', 'abp.services.app.assetHealth',
+        function ($scope, $location, $uibModal, assetService, variableService, assetHealthService) {
             var vm = this;
             vm.localize = abp.localization.getSource('AssetManager');
+            vm.selectedVariable = {name: '?'};
 
             vm.gridOptions = {
                 data: [],
@@ -20,16 +22,56 @@
             };
             function registerGridApi(gridApi) { vm.gridApi = gridApi; }
 
-            vm.refresh = function () {
-                abp.ui.setBusy( //Set whole page busy until getAssets completes
-                    null,
-                    assetHealthService.getAssetVariableList({})
-                        .success(function (data) {
-                            vm.gridOptions.data = data.assetVariables;
-                        })
-                );
+            vm.getData = function () {
+                vm.variables = [];
+                variableService.getAllVariables({})
+                    .success(function (data) {
+                        vm.variables = data.variables;
+                    });
+                vm.assetHierarchy = [];
+                assetService.getAssetHierarchyAsList({})
+                    .success(function (data) {
+                        for (var i = 0; i < data.assetHierarchy.length; i++)
+                            data.assetHierarchy[i].$$treeLevel = data.assetHierarchy[i].level;
+                        vm.assetHierarchy = data.assetHierarchy;
+                    });
+                vm.assetVariables = [];
+                assetHealthService.getAssetVariableList({})
+                    .success(function (data) {
+                        vm.assetVariables = data.assetVariables;
+                    });
             };
-            vm.refresh();
+
+            vm.open = function () {
+                var modalInstance = $uibModal.open({
+                    templateUrl: 'myModalContent.html',
+                    controller: modalControllerId,
+                    resolve: { variables: function () { return vm.variables; } }
+                });
+
+                modalInstance.result.then(function (selectedVariable) {
+                    vm.selectedVariable = selectedVariable;
+                });
+            }
+
+            vm.getData();
         }
     ]);
+
+    app.controller(modalControllerId, [
+        '$scope', '$uibModalInstance', 'variables',
+        function ($scope, $uibModalInstance, variables) {
+            $scope.variables = variables;
+            $scope.selected = {
+                variable: $scope.variables[0]
+            };
+
+            $scope.ok = function () {
+                $uibModalInstance.close($scope.selected.variable);
+            };
+
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+    }]);
 })();
